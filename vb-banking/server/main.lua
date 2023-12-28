ae8d6da7373c8086b9d6bf5f720814080874c0ca
SP1D3R <120335846+SP1D3R777@users.noreply.github.com>
2023-12-28 17:46:53 +0100

    Add files via upload

---
+++ vb-banking/server/main.lua
@@ -0,0 +1,74 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterServerCallback('vb-banking:server:GetPlayerName', function(source, cb)
+	local _char = ESX.GetPlayerFromId(source)
+	local _charname = _char.getName()
+	cb(_charname)
+end)
+
+RegisterServerEvent('vb-banking:server:depositvb')
+AddEventHandler('vb-banking:server:depositvb', function(amount, inMenu)
+	local _src = source
+	local _char = ESX.GetPlayerFromId(_src)
+	amount = tonumber(amount)
+	Citizen.Wait(50)
+	if amount == nil or amount <= 0 or amount > _char.getMoney() then
+		TriggerClientEvent('chatMessage', _src, "Mauvais montant.")
+	else
+		_char.removeMoney(amount)
+		_char.addAccountMoney('bank', tonumber(amount))
+		_char.showNotification("Vous avez déposé  $"..amount)
+	end
+end)
+
+RegisterServerEvent('vb-banking:server:withdrawvb')
+AddEventHandler('vb-banking:server:withdrawvb', function(amount, inMenu)
+	local _src = source
+	local _char = ESX.GetPlayerFromId(_src)
+	local _base = 0
+	amount = tonumber(amount)
+	_base = _char.getAccount('bank').money
+	Citizen.Wait(100)
+	if amount == nil or amount <= 0 or amount > _base then
+		TriggerClientEvent('chatMessage', _src, "Invalid Quantity")
+	else
+		_char.removeAccountMoney('bank', amount)
+		_char.addMoney(amount)
+		_char.showNotification("Vous avez retiré $"..amount)
+	end
+end)
+
+RegisterServerEvent('vb-banking:server:balance')
+AddEventHandler('vb-banking:server:balance', function(inMenu)
+	local _src = source
+	local _char = ESX.GetPlayerFromId(_src)
+	local balance = _char.getAccount('bank').money
+	TriggerClientEvent('vb-banking:client:refreshbalance', _src, balance)
+end)
+
+RegisterServerEvent('vb-banking:server:transfervb')
+AddEventHandler('vb-banking:server:transfervb', function(to, amountt, inMenu)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local zPlayer = ESX.GetPlayerFromId(tonumber(to))
+	local balance = 0
+	if zPlayer ~= nil then
+		balance = xPlayer.getAccount('bank').money
+		if tonumber(_source) == tonumber(to) then
+			TriggerClientEvent('chatMessage', _source, "Vous ne pouvez pas transferer de l'argent à vous même !")	
+		else
+			if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
+				TriggerClientEvent('chatMessage', _source, "Vous n'avez pas assez d'argent sur votre compte")
+			else
+				xPlayer.removeAccountMoney('bank', tonumber(amountt))
+				zPlayer.addAccountMoney('bank', tonumber(amountt))
+				zPlayer.showNotification("Vous avez reçu un virement de  "..amountt.."$ par l'ID: ".._source)
+				xPlayer.showNotification("Vous avez envoyé un virement de "..amountt.."$ à l'ID: "..to)
+			end
+		end
+	else
+		TriggerClientEvent('chatMessage', _source, "Cet ID n'est pas valide !")
+	end
+end)
